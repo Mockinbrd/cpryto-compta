2680ff8cc872b957e3706525924072b79edb693e
Mockinbrd <marvyn.ap@gmail.com>
2021-01-06 20:54:58 +0100

    feat: User entity + fixtures feat: Service Mailer

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
